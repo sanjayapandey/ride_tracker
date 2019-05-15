c6148bd65d07e172363be3218b0efa7448f5f0b0
sanjayapandey <pandeysanjaya4@gmail.com>
2019-05-15 14:29:21 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/service/RideService.java
@@ -0,0 +1,12 @@
+package com.pluralsight.service;
+
+import java.util.List;
+
+import com.pluralsight.model.Ride;
+
+public interface RideService {
+
+	Ride createRide(Ride ride);
+	List<Ride> getRides();
+
+}
